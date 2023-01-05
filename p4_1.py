cca7d53e708c2abcac0c94f0baca0e07554a56e2
Akilax0 <killerilaksha@gmail.com>
2023-01-05 08:03:36 +0530

    Object approach init

--- p4_1.py
+++ p4_1.py
@@ -6,5 +6,22 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+# init a layer 
+# two ways
+# trained model you saved
+# you save the weights and the biases
+# when initializaing load them
+# And when saving just saving the weights and the biases
+# 
+# New neural netowrk
+# weights initialized between -1 & 1
+# so normalize and scale dataset
+# biases tend to 0
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
 
 
